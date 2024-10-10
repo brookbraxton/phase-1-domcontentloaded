2aa2983e6b5c4210ba6f7ecbc344e317868d52c3
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-10-10 21:36:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    // Target the paragraph element with id="text"
+    const textElement = document.getElementById('text');
+    
+    // Replace its content
+    textElement.textContent = "This is really cool!";
+});
+
